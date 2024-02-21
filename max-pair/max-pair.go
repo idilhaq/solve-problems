40017996ca018fdb9678ec5a0b149777f2e3d69b
Muhammad Idil Haq Amir <idilhaq08@gmail.com>
2024-02-21 12:29:47 +0700

    max pair alt

--- max-pair/max-pair.go
+++ max-pair/max-pair.go
@@ -1,4 +1,4 @@
-package maxpair
+package main
 
 import (
 	"fmt"
@@ -32,4 +32,34 @@ func MaxPair() {
 	}
 
 	fmt.Println("Expected max pair is:", maxPairs)
+}
+
+func MaxPairAlt() {
+	input := [][]int{{1, 2}, {2, 3}, {2, 1}, {2, 1}, {1, 2}, {2, 1}, {1, 2}, {2, 2}, {2, 2}, {3, 2}}
+
+	pairSetDetail := make(map[[2]int]int)
+
+	for _, pair := range input {
+		if pair[1] > pair[0] {
+			pair[0], pair[1] = pair[1], pair[0]
+		}
+
+		pairStr := [2]int{pair[0], pair[1]}
+
+		pairSetDetail[pairStr]++
+	}
+
+	maxPairs := 0
+	for _, pairCount := range pairSetDetail {
+		if pairCount >= 2 {
+			maxPairs += pairCount - (pairCount % 2)
+		}
+	}
+
+	fmt.Println("Expected max pair is:", maxPairs/2)
+}
+
+func main() {
+	MaxPair()
+	MaxPairAlt()
 }
